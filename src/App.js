e284d1110f367ecd04ebed15f3a2450e7a8b750f
wownsl456 <wownsl456@naver.com>
2021-03-25 23:26:52 +0900

    first React App

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 import React from 'react';
+import axios from "axios";
 
-function Food({ name, picture }) {
-    return <div>
-    <h2>I like {name}</h2>
-    <img src= {picture} alt={name} />
-    </div>
-}
-
-const foodILike = [
-    {
-        id:1,
-        name: "Kimchi",
-        image:
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7-rpW2ynK3Mq89GWd2Q36QHaE8%26pid%3DApi&f=1"
-    },
-    {
-        id:2,
-        name: "Samgyeopsal",
-        image:
-            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic.news.zumst.com%2Fimages%2F2%2F2017%2F02%2F26%2F2030c76e427f4c278c16ab0eb81bd45d.jpg&f=1&nofb=1"
-    },
-    {
-        id:3,
-        name: "Bibimbap",
-        image: 
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.B3ddBez0ci1Fga_TDPmFhwHaEK%26pid%3DApi&f=1"
-    },
-    {
-        id:4,
-        name: "Doncasu",
-        image: 
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.CjPaLIAcKIH3n7_xK3dK7AAAAA%26pid%3DApi&f=1"
-    },
-    {
-        id:5,
-        name: "Kimbap",
-        image: 
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.muoVibDUIawORHUPymUWfQHaEK%26pid%3DApi&f=1"
+class App extends React.Component{
+    state = {
+        isLoading: true,
+        movies: []
+    };
+    getMovies = async () => {
+        const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
     }
-];
 
-function App() {
-  return (
-      <div>
-      {foodILike.map(dish => (
-      <Food key={dish.id} name={dish.name} picture={dish.image} />
-      ))}
-      </div>
-  );
+    componentDidMount(){
+        this.getMovies();
+    }
+    render(){
+        const { isLoading } = this.state;
+        return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
+    }   
 }
 
 export default App;
